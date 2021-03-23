11c8d48d7972c08d9e4d57c6e14aeb4279831c49
higorbds <h.higorbsilva.dev@gmail.com>
2021-03-23 10:45:05 -0300

    Criando o projeto da Primeira aula

---
+++ conversor-moedas/js/main.js
@@ -0,0 +1,28 @@
+const button = document.querySelector('.botao__principal')
+const popupWrapper = document.querySelector('.popup-wrapper')
+const popupClose = document.querySelector('.popup-close')
+const resultElement = document.querySelector('.popup .result')
+
+
+
+button.addEventListener('click', () => {
+  const dolarInput = document.querySelector('.conversor input').value
+  const dolarInputNum = parseFloat(dolarInput);
+  const dolarValue = 5.51
+  
+  const result = dolarInputNum * dolarValue
+  const resultFormated = result.toFixed(2).toString().replace('.', ',')
+
+  resultElement.innerHTML = "R$" + resultFormated
+
+  popupWrapper.style.display = 'block'
+})
+
+
+popupWrapper.addEventListener('click', e => {
+  const classOfElementClicked = e.target.classList[0];
+
+  if(classOfElementClicked === 'popup-wrapper' || classOfElementClicked === 'popup-close') {
+    popupWrapper.style.display = 'none'
+  }
+})
